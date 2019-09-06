428dfd3f38b6a6f93a050a0bc816f0562a3f2a46
Dominik Wojciechowski <dominik.woj@me.com>
2019-09-06 15:01:54 +0200

    build site

--- Gemfile
+++ Gemfile
@@ -14,6 +14,9 @@ gem 'sassc-rails'
 gem 'simple_form'
 gem 'uglifier'
 gem 'webpacker'
+gem 'clipboard-rails'
+gem 'turbolinks'
+
 
 group :development do
   gem 'web-console', '>= 3.3.0'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,22 @@
 //= require rails-ujs
 //= require_tree .
+//= require clipboard
+//= require turbolinks
+
+const button = document.getElementById('copy-button')
+
+const changeText = () => {
+  button.addEventListener('click', (event) => {
+    event.currentTarget.innerText = "Copied!"
+  });
+}
+
+changeText();
+
+
+const copyClipboard = () => {
+  var clipboard = new Clipboard('.clipboard-btn');
+  console.log(clipboard);
+}
+
+copyClipboard();
